cc89d59dd6d6be2c5d4f8c2d4b7f71e874744d1c
Phillip Eis <45538799+PhillipEis@users.noreply.github.com>
2024-01-03 13:46:35 +0100

    Edit files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'visualzx'
+author 'Visualz Development <https://visualz.dk>'
 description 'Accountant Job'
 version '1.0'
 
 shared_script { "@es_extended/imports.lua", "@ox_lib/init.lua", "config.lua" }
-client_scripts { "client.lua" }
-server_scripts { "@oxmysql/lib/MySQL.lua", "server.lua", "logs.lua" }
+client_scripts { "client/client.lua" }
+server_scripts { "@oxmysql/lib/MySQL.lua", "server/server.lua", "server/logs.lua" }
+
+escrow_ignore { "server/logs.lua" }
